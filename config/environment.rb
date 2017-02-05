7a053a09720fd757305918299e9f33e3fda86b15
Daniel Joyce <dajoyce92@gmail.com>
2017-02-05 21:19:09 +0000

    fixed environment rb

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,12 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings={
-    :port       =>    ENV['MAILGUN_SMTP_PORT'],
-    :address    =>    ENV['MAILGUN_SMTP_SERVER'],
-    :user_name  =>  ENV['MAILGUN_SMTP_LOGIN'],
-    :password   => ENV['MAINGUN_SMTP_PASSWORD'],
-    :domain     => 'mysterious-coast-38467.herokuapp.com/',
-    :authentication => :plan,
+    :port           =>    ENV['MAILGUN_SMTP_PORT'],
+    :address        =>    ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      =>    ENV['MAILGUN_SMTP_LOGIN'],
+    :password       =>    ENV['MAINGUN_SMTP_PASSWORD'],
+    :domain         =>    'mysterious-coast-38467.herokuapp.com/',
+    :authentication =>    :plain,
 }
 
 ActionMailer::Base.delivery_method = :smtp
