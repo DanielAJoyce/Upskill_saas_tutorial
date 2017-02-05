fdaf3d73741a13b9b7019aa99ca828ff6bfabeb8
Daniel Joyce <dajoyce92@gmail.com>
2017-02-05 21:44:04 +0000

    added comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,11 +1,14 @@
 class ContactsController < ApplicationController
+   
+   # Creates instance of Contact object (Get request to /contact-us)
+   # Show new contact form
    def new
       @contact = Contact.new
    end
    
    # creates instance of Contact object with parameters. 
    def create
-      @contact = Contact.new(contact_params)
+      @contact = Contact.new(contact_params) # assigns form fields into Contact Object
       if  @contact.save 
          #if @contact form saves correctly, redirect to new contact/new and give them a message
          
@@ -13,14 +16,18 @@ class ContactsController < ApplicationController
          name = params[:contact][:name]
          email = params[:contact][:email]
          body = params[:contact][:comments]
+         
          # params is how you can access those values.
          
-         #send these variables to contact_email
+         #email method and send mail
          ContactMailer.contact_email(name, email, body).deliver
          
+         #Store success message in flash hash
          flash[:success] = "message sent."
+         #redirect to new action
          redirect_to contact_us_path
       else
+         # Stores errors in flash hash and redirect to new action.
          flash[:danger] = @contact.errors.full_messages.join(", ") # creates clean error message
          redirect_to contact_us_path
       end
@@ -30,8 +37,10 @@ class ContactsController < ApplicationController
    
       #private function
    private 
+   
+   #To collect data from form, we need ot use
+   #Strong parameters and whitelist the form fields.
       def contact_params
-         #Security functionality
          params.require(:contact).permit(:name, :email, :comments)
       end
 end
