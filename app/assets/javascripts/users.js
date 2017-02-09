e520492263d79cdfafebe3dca4f088cd9e99b5e9
Daniel Joyce <dajoyce92@gmail.com>
2017-02-09 21:38:10 +0000

    I fixed the Stripe Issue, I hate myself right now. 4 HOURS WHY

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,89 +1,58 @@
-//Document Ready
-
-
-
-
-
-/* global $ */ // this tells it that jquery is already imported elsewhere.
-/* global Stripe */ 
-
-$(document).on('turbolinks:load', function() {
-    
-    var theForm = $('#pro_form');
-    var submitBtn = $('#form-submit-btn');
-    
-    //Set Stripe Public Key
-    Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content'));
-    
-    
-    // When user clicks form submit button
-    submitBtn.click(function(event){
-        //prevent default submission behavior.
+/* global $, Stripe */
+//Document ready.
+$(document).on('turbolinks:load', function(){
+  var theForm = $('#pro_form');
+  var submitBtn = $('#form-signup-btn');
+  //Set Stripe public key.
+  Stripe.setPublishableKey( $('meta[name="stripe-key"]').attr('content') );
+  //When user clicks form submit btn,
+  submitBtn.click(function(event){
+    //prevent default submission behavior.
     event.preventDefault();
-    
-    submitBtn.val('Processing').prop('disabled', true);
-    
-        // collect credit card fields
+    submitBtn.val("Processing").prop('disabled', true);
+    //Collect the credit card fields.
     var ccNum = $('#card_number').val(),
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
-    
-    // Use Stripe JS library to check for card errors
-    
+    //Use Stripe JS library to check for card errors.
     var error = false;
-    
-    //Validate Card Number
-   if(!Stripe.card.validateCardNumber(ccNum))
-   {
-       
-       error = true;
-       alert('The credit card number is not valid');
-   }
-   
-   //Validate CVC Number
-   if(!Stripe.card.validateCVC(cvcNum))
-   {
-       error = true;
-       alert('The CVC is not correct.');
-   }
-   // validate expiration date.
-   if(!Stripe.card.validateExpiry(expMonth, expYear))
-   {
-       error = true;
-       alert('The Expiration date is incorrect.');
-   }
-   
-    
-        // send card info to stripe
-    if (error){
-        submitBtn.val('Sign Up').prop('disabled', false);
+    //Validate card number.
+    if(!Stripe.card.validateCardNumber(ccNum)) {
+      error = true;
+      alert('The credit card number appears to be invalid');
     }
-    else
-    {
-       Stripe.createToken({
+    //Validate CVC number.
+    if(!Stripe.card.validateCVC(cvcNum)) {
+      error = true;
+      alert('The CVC number appears to be invalid');
+    }
+    //Validate expiration date.
+    if(!Stripe.card.validateExpiry(expMonth, expYear)) {
+      error = true;
+      alert('The expiration date appears to be invalid');
+    }
+    if (error) {
+      //If there are card errors, don't send to Stripe.
+      submitBtn.prop('disabled', false).val("Sign Up");
+    } else {
+      //Send the card info to Stripe.
+      Stripe.createToken({
         number: ccNum,
         cvc: cvcNum,
         exp_month: expMonth,
         exp_year: expYear
-    }, stripeResponseHandler);
-          
-          return false;  
-       
+      }, stripeResponseHandler);
     }
-    }); 
-    // stripe returns card token
-       function stripeResponseHandler(status, response)
-       {
-           var token = response.id; 
-       //inject card token into hidden field.
-       
-            theForm.append($('<input type="hidden"name ="user[stripe_card_token]">').val(token));
-               
-           
-// submit form to rails app
-            theForm.get(0).submit();
-       }
-            
-
+    return false;
+  });
+  //Stripe will return a card token.
+  function stripeResponseHandler(status, response) {
+    //Get the token from the response.
+    var token = response.id;
+    //Inject the card token in a hidden field.
+    theForm.append( $('<input type="hidden" name="user[stripe_card_token]">').val(token) );
+    //Submit form to our Rails app.
+    theForm.get(0).submit();
+  }
 });
