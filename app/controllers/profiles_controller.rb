68845bf767e26fb7ee255289ffa8fd5cb4062b6f
Daniel Joyce <dajoyce92@gmail.com>
2017-02-10 21:18:28 +0000

    added creation form for profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,11 +1,7 @@
 class ProfilesController < ApplicationController
-
-
     # GET to /users/:user_id/profile/new 
-
     def new
-    
     #When this happens, render blank profile details form.
+    @profile = Profile.new
     end
-
 end
